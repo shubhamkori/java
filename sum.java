388118456db2189d9da87ed7fc0d1b831755b319
shubham <shubhamkori310@gmail.com>
2022-03-30 16:51:15 +0530

    this is pass

---
+++ sum.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class sum {
+
+    static int findSum(int n)
+    {
+        int sum = 0;
+        for (int x = 1; x <= n; x++)
+            sum = sum + x;
+        return sum;
+    }
+ 
+    public static void main(String args[])
+    {
+        int n = 5;
+        System.out.println(findSum(n));
+    }
+    
+}
