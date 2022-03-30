c66be3c4a8de3b6a8da2b7603eac91fb99c81a4a
shubham <shubhamkori310@gmail.com>
2022-03-30 17:08:28 +0530

    This is a last assignment project

---
+++ check_function.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class check_function {
+    
+    public static void main(String[] args)
+    {
+        List<String> check_function = new ArrayList<String>();
+  
+        check_function.add("Welcome");
+        check_function.add("To");
+        check_function.add("my");
+        check_function.add("home");
+        check_function.add("on jabalpur");
+  
+        System.out.println(check_function.contains("Welcome")); 
+        System.out.println(check_function.contains("Java")); 
+    }
+}
